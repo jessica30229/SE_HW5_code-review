50c2d153d48e335102a37d9824548080b84fa967
Jessica Jiang <jessica30229@gmail.com>
2021-05-27 23:28:35 +0800

    add {} 225 232

--- Shell_Sort_and_Quick_Sort.cpp
+++ Shell_Sort_and_Quick_Sort.cpp
@@ -216,12 +216,12 @@ void quickSort( vector<collegeType> &aSet, string fileName, float rTime ) { // d
  	timer = clock(); 	// start the timer	
  	
 	QuickSortRecursive( tempS, 0, tempS.size() - 1, choice1 );	// �Ĥ@���q�Ƨ� 
-	if ( show1 == '-' )	// �ƧǤ覡�� ����
+	if ( show1 == '-' )	{// �ƧǤ覡�� ����
 		vector<collegeType> temp(tempS);
 		for ( int i = 0; i < tempS.size(); i++ )
 			tempS[i] = temp[tempS.size() - i - 1];
 		// if
-	
+	}
 	if ( choice2 == choice1 ) {		// �n�ƪ�������O�@�˪� ( EX�G�H�� �H�� ) 
 		if ( ( show1 == '-' && show2 == '+' ) || (show1 == '+' && show2 == '-' ) ) {	
 			// ���ƧǤ覡���P�� ( ��J�G1 + 1 - ) 
